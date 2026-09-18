fa8807d9ad61e7ed51369a7879a116ea2ded3ae9
Ryan Ramage <r@mage.rocks>
2026-09-18 08:59:34 -0600

    updater-error and unknown-message events

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ const store = new Corestore(path.join(updaterConfig.dir, 'pear-runtime', 'corest
 const swarm = new Hyperswarm()
 const pear = new PearRuntime({ ...updaterConfig, swarm, store })
 
-// pear.updater.on('error', console.error)
+pear.updater.on('error', (err) => pipe.write('updater-error ' + err.message))
+
 if (updaterConfig.updates !== false) {
   swarm.on('connection', (connection) => store.replicate(connection))
   swarm.join(pear.updater.drive.core.discoveryKey, {
@@ -53,7 +54,10 @@ goodbye(async () => {
 
 pipe.on('data', async (data) => {
   const message = data.toString()
-  if (message !== 'pear:applyUpdate') return // else console.log(message)
+  if (message !== 'pear:applyUpdate') {
+    pipe.write('unknown-message ' + message)
+    return
+  }
 
   try {
     await pear.ready()
